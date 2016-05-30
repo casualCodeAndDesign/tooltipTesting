d177e49dc49fc1f27988dfd79d2769a7b3a484df
Joni Kämppä <johnniecasual@gmail.com>
2016-05-30 07:00:18 +0300

    Ajax Files

---
+++ tooltipTesting/Scripts/toolTipLoader.js
@@ -0,0 +1,23 @@
+﻿$('[data-toggle="tooltip"]').each(function () {
+    var self = this;
+    $.ajax({
+        url: '/Content/tooltip.xml',
+        type: 'get',
+        dataType: 'xml',
+        success: function(data, textStatus)
+        {
+            console.log("DATA IS: " + data);
+            var content = $(data).find('tooltip[id="' + self.id + '"]').text();
+            console.log("THIS IS THE CONTENT NOW: " + content);
+            console.log("This is the content: " + content)
+            // Parse the content
+            console.log("THIS ID: " + self.id);
+            //var content = $(data).find(self.id).text();
+            console.log("################################# TOOLTIP CONTENT ###################################");
+            console.log(content);
+            content = content.trim();
+            //  Update the tooltip content
+            $(self).prop('title', content);
+        }
+    })
+})
---
+++ tooltipTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tooltipTesting.Startup))]
+namespace tooltipTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
